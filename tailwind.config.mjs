0ba0be8ee1b566e73d96334cde53aea3318adabb
sebadio <sebastiandiazdopazo@gmail.com>
2024-07-15 12:44:35 -0300

    finished ui

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			borderWidth: {
+				1: "1px"
+			},
 			colors: {
 				background: 'hsl(211, 100%, 6%)',
 				custom: {
